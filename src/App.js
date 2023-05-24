c78a583b58fafb7f4ac8f829b48f71d898102ae5
feehaam <mdfeeham@gmail.com>
2023-05-24 19:42:57 +0600

    Home frame setup done

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Topbar from './components/global/Topbar';
+import LeftBar from './components/global/LeftBar';
+import Diet from './components/pages/Diet';
+import Exercise from './components/pages/Exercise';
+import TotalStats from './components/pages/TotalStats';
+import Plans from './components/pages/Plans';
+import Guides from './components/pages/Guides';
+import Nutritions from './components/pages/Nutritions';
+import Workouts from './components/pages/Workouts';
+import Profile from './components/pages/Profile';
+import Dashboard from './components/pages/Dashboard';
+import Settings from './components/pages/Settings';
+import RightBar from './components/global/RightBar';
+import { useState } from 'react';
+import { isMobile } from 'react-device-detect';
+
+function App() {
+
+  const [theme, colorMode] = useMode();
+  const [showRightbar, setShowRightbar] = useState(!isMobile);
+  const mode = theme.palette.mode;
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <LeftBar />
+          <main className='content'>
+            <Topbar showRightbar={showRightbar} setShowRightbar={setShowRightbar} />
+            {showRightbar ? <RightBar showRightbar={showRightbar} setShowRightbar={setShowRightbar} /> : ""}
+            <div style={{
+              borderLeft: mode === 'dark' ? '1px solid #555' : '1px solid #ccc',
+              borderBottom: mode === 'dark' ? '1px solid #555' : '1px solid #ccc',
+              padding: '15px'
+            }}>
+              <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path="/diet" element={<Diet />} />
+                <Route path="/exercise" element={<Exercise />} />
+                <Route path="/total-stats" element={<TotalStats />} />
+                <Route path="/plans" element={<Plans />} />
+                <Route path="/guides" element={<Guides />} />
+                <Route path="/nutritions" element={<Nutritions />} />
+                <Route path="/workouts" element={<Workouts />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/settings" element={<Settings />} />
+              </Routes>
+            </div>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/global/Topbar.jsx
@@ -0,0 +1,97 @@
+import { Box, IconButton, useTheme, Typography } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens} from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import TuneRoundedIcon from '@mui/icons-material/TuneRounded';
+import MenuOpenRoundedIcon from '@mui/icons-material/MenuOpenRounded';
+import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
+import {isMobile} from 'react-device-detect';
+
+const Topbar = ({ showRightbar, setShowRightbar }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  const mode = theme.palette.mode;
+
+  function handleRightbar() {
+    if (showRightbar) setShowRightbar(false);
+    else setShowRightbar(true);
+  }
+
+  return (
+    <>
+      <Box display="flex" justifyContent="space-between" p={2}  sx={{
+          boxShadow: mode === 'dark' ? '#778 0px 1px 4px 0px' : '#aaa 0px 1px 4px 0px', 
+          position: 'sticky',
+          backgroundColor: mode === 'dark' ? colors.primary[500] : 'white',
+          top: 0
+        }}>
+        {/* SEARCH BAR */}
+        <Box
+          display="flex"
+        >
+          <Typography variant={isMobile ? 'h4' : 'h2'} color='#ff5544' sx={{
+            fontWeight: 'bold'
+          }}>
+            <IconButton onClick={handleRightbar}>
+              <img src="../../../favicon.ico" />
+            </IconButton>
+            FitnessBuddy
+          </Typography>
+        </Box>
+
+        {/* ICONS */}
+        <Box display="flex">
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+          {/* <IconButton>
+            <TuneRoundedIcon />
+          </IconButton> */}
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <PersonOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppRoundedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+      {showRightbar ?
+        ''
+        :
+        <Box display="flex" justifyContent="right" mr={'25px'} onClick={handleRightbar} 
+        sx={{
+          borderLeft: mode === 'dark' ? '1px solid #555' : '1px solid #ccc',
+          }}>
+          <Box textAlign="center">
+            <Typography className="hoverer"
+              variant="h3"
+              color={colors.grey[100]}
+              fontWeight="bold"
+              sx={{ m: "10px 0 0 0" }}
+              backgroundColor={colors.primary[400]}
+              borderRadius="3px"
+              p={1}>
+              Add daily activity
+              <IconButton>
+                <MenuOpenRoundedIcon />
+              </IconButton>
+            </Typography>
+          </Box>
+        </Box>
+      }
+    </>
+  );
+};
+
+export default Topbar;
